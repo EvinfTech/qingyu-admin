2ee294e934ab99fc3ad2675f2680094ed8f611c8
xushenglong <xushenglong1997@qq.com>
2023-11-07 17:08:50 +0800

    <添加功能>订场页列表样式完成

--- src/views/site/Site.vue
+++ src/views/site/Site.vue
@@ -2,32 +2,42 @@
     <div style="height: 100%">
         <el-row class="outer">
 
-            <!-- <el-col :span="1">
-                <div style="height: 0.2rem;"></div>
-                <p v-for="hour in hourList" class="text-hour">{{ hour }}</p>
-            </el-col> -->
-
             <el-col :span="19">
-                <el-table :data="tableData" style="width: 100%" size="large" :cell-style="cell2">
-                    <el-table-column prop="time" class="text-hour"  align="center"/>
-                    <el-table-column v-for="ground in groundList"
-                    :prop="ground"
-                    :label="ground"
-                    :width="itemWidth"
-                    align="center">
-                        <template #default="scope">
-                            <div v-if="scope.row[ground] === 1" class="cell3 online" >
+                <div class="header-row">
+                    <div v-for="header in groundList" class="cell4">
+                        <span class="header-cell">{{ header }}</span>
+                </div>
+                </div>
+                <template v-for="ground in tableData" :key="ground.time">
+                    <!-- <template #default="scope"> -->
+                    <div style="width: 100%" class="cell-row">
+                        <div class="cell-time">
+                            {{ ground.time }}
+                        </div>
+                        <div v-for="i in groundList" class="cell4">
+                            <div v-if="isOvertime(ground.time)" class="cell3 overtime" >
+                                <span class="text-online">￥100</span>
+                            </div>
+                            <div v-else-if="ground[i] == 1" class="cell3 online" >
                                 <span class="text-online">线上预定</span>
                             </div>
-                            <div v-else-if="scope.row[ground] === 2" class="cell3 offline" >
+                            <div v-else-if="ground[i] == 2" class="cell3 offline" >
                                 <span class="text-offline">线下预定</span>
                             </div>
-                            <div v-else class="cell3">
+                            <div v-else-if="isChose(ground, i)" class="cell3 cell3-chose" >
+                                <span class="cell3-empty">￥1002</span>
+                            </div>
+                            <div v-else class="cell3 cell3-empty-ct" @click="() => choose(ground, i)">
                                 <span class="cell3-empty">￥100</span>
                             </div>
-                        </template>
-                    </el-table-column>
-                </el-table>
+                        </div>
+                    </div>
+                </template>
+                <div style="width: 100%" class="cell-row">
+                    <div class="cell-time">
+                            {{ tableData[tableData.length-1].time }}
+                    </div>
+                </div>
             </el-col>
 
             <el-col :span="1" style="background-color: #FFFFFF" />
@@ -82,7 +92,7 @@
                                 </el-col>
                             </el-row>
                             <el-row>
-                                <el-button type="primary" @click="onSubmit" style="width: 100%">结算</el-button>
+                                <el-button type="primary" @click="onSubmit" style="width: 90%">结算</el-button>
                             </el-row>
                         </div>
                     </el-form>
@@ -98,6 +108,9 @@
 import { ref, reactive } from 'vue'
 import type { Ref } from 'vue'
 
+var currentHour = (new Date()).getHours();
+
+
 const grounds = [1, 2, 3]
 
 const _items :Ref<Number[]> = ref([])
@@ -108,6 +121,14 @@ const form = reactive({
   remark: '',
   items: _items,
 })
+
+const isOvertime = (startTime: string) => {
+    let hour = parseInt(startTime.substring(0, 3));
+    if (hour <= currentHour) {
+        return true;
+    }
+    return false;
+}
 
 const chooseAll = () => {
     _items.value = grounds;
@@ -130,19 +151,13 @@ const groundList = [
     "10号场"
 ];
 
-const itemWidth = (1700 / groundList.length) + "vw";
+const itemSpan = 24 / (groundList.length+1);
 
-function cell2() {
-    return {
-        "padding-left": 0,
-        "padding-right": 0,
-        "padding-top": "0.2rem",
-        "padding-bottom": "0.2rem",
-    };
+interface Data {
+    [index: string]: string|number,
 }
 
-
-const tableData = [
+const tableData : Data[] = [
     {
         "time": "06:00",
         "1号场": 1,
@@ -155,7 +170,7 @@ const tableData = [
         "8号场": 1,
     },
     {
-        "time": "06:00",
+        "time": "07:00",
         "1号场": 1,
         "2号场": 1,
         "3号场": 1,
@@ -166,7 +181,7 @@ const tableData = [
         "8号场": 1,
     },
     {
-        "time": "06:00",
+        "time": "08:00",
         "1号场": 1,
         "2号场": 1,
         "3号场": 2,
@@ -177,16 +192,17 @@ const tableData = [
         "8号场": 1,
     },
     {
-        "time": "06:00",
+        "time": "09:00",
         "3号场": 1,
         "4号场": 1,
         "5号场": 1,
     },
     {
-        "time": "06:00",
+        "time": "10:00",
         "2号场": 1,
     },
     {
+        "time": "11:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -197,6 +213,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "12:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -207,6 +224,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "13:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 2,
@@ -217,6 +235,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "14:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -227,6 +246,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "15:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -237,6 +257,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "16:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -247,6 +268,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "17:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -257,6 +279,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "18:00",
         "1号场": 0,
         "2号场": 2,
         "3号场": 0,
@@ -267,6 +290,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "19:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -277,6 +301,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "20:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -287,6 +312,7 @@ const tableData = [
         "8号场": 1,
     },
     {
+        "time": "21:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 1,
@@ -297,6 +323,7 @@ const tableData = [
         "8号场": 0,
     },
     {
+        "time": "22:00",
         "1号场": 0,
         "2号场": 0,
         "3号场": 0,
@@ -305,8 +332,46 @@ const tableData = [
         "6号场": 0,
         "7号场": 0,
         "8号场": 0,
-    },
+    }
 ];
+
+const choseList : Data[] = []
+
+const choose = (data: Data, index: string) => {
+    console.log(data);
+    console.log(index);
+    let flag = false;
+    choseList.forEach((item) => {
+        console.log(item);
+        
+        if (item.time === data.time) {
+            item[index] = 5;
+            flag = true;
+            return;
+        }
+    })
+    if (!flag) {
+        choseList.push({
+        time: data.time,
+        [index]: 5,
+    })
+    }
+    console.log(choseList);
+    
+}
+
+const isChose = (data: Data, index: string) => {
+    choseList.forEach((item) => {
+        if (item.time == data.time) {
+            if (item[index] == 5) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    })
+}
+
 </script>
 
 <style scoped>
@@ -314,13 +379,34 @@ const tableData = [
     height: 100%;
     background-color: #f6f8fa;
 }
-.text-hour{
-    margin-top: 2.6rem;
-    margin-bottom: 0rem;
-    margin-left: 3rem;
+.header-row{
+    width: 94%;
+    margin-left: 6%;
+    display: flex;
+}
+.header-cell{
+    height: 4vh;
+    margin: 0.3vh 0.2vw;
+    padding:0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.cell-row{
+    display: flex;
+    justify-content: space-between;
+}
+.cell-time{
+    width: 6%;
+    margin-top: -0.5%;
+    padding-left: 3%;
+}
+.cell4{
+    width: 10%;
 }
     .cell3 {
-        height: 5vh;
+        height: 4vh;
+        margin: 0.3vh 0.2vw;
         padding:0;
         display: flex;
         align-items: center;
@@ -332,14 +418,21 @@ const tableData = [
         font-size: 15px;
         color: #9E9E9E;
     }
+    .cell3-empty-ct:hover{
+        border: solid 2px;
+        border-color: #0077FF;
+    }
     .online{
         background: #FFC300;
     }
     .text-online{
         color: #FFFFFF;
     }
-    .offline {
+    .offline{
         background: #0077FF;
+    }
+    .overtime{
+        background: #D8D8D8;
     }
     .text-offline{
         font-size: 15px;
@@ -385,7 +478,7 @@ const tableData = [
         padding-top: 6px;
     }
     .form-bottom{
-        width: 13vw;
+        width: 13%;
         position: fixed;
         bottom: 2vh; 
         margin-left: 2vw;
