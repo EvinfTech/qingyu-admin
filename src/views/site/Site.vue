11c29315c241dee18cd0bdc550aed01148eff53e
Yin <775709948@qq.com>
2023-12-08 11:40:54 +0800

    <完善功能>: 修改黄色叹号

--- src/views/site/Site.vue
+++ src/views/site/Site.vue
@@ -3,38 +3,51 @@
     <div class="left">
       <div class="left-top">
         <h3>订场面板</h3>
-        <div class="example-container" style="background-color: #0077FF;"/>
+        <div class="example-container" style="background-color: #0077ff" />
         <span class="text-example">线下预定</span>
 
-        <div class="example-container" style="background-color: #FFC300;"/>
+        <div class="example-container" style="background-color: #ffc300" />
         <span class="text-example">线上预定</span>
 
-        <div class="example-container" style="background-color: #D8D8D8;"/>
+        <div class="example-container" style="background-color: #d8d8d8" />
         <span class="text-example">不可预定</span>
 
-        <div class="example-container" style="background-color: #EFF3FF;"/>
+        <div class="example-container" style="background-color: #eff3ff" />
         <span class="text-example">可预定</span>
 
-        <div class="example-container example-container-chose" style="background-color: #EFF3FF;">
-                        <span class="tick">
-                            <el-icon size="10"><Check/></el-icon>
-                        </span>
+        <div
+          class="example-container example-container-chose"
+          style="background-color: #eff3ff"
+        >
+          <span class="tick">
+            <el-icon size="10"><Check /></el-icon>
+          </span>
         </div>
         <span class="text-example">已选择</span>
       </div>
       <div class="left-main">
         <div class="date-select">
-          <el-button round v-for="n in 7" :key="n" :class="{ active: n == upBtn }" @click="dateButton(n-1)">
+          <el-button
+            round
+            v-for="n in 7"
+            :key="n"
+            :class="{ active: n == upBtn }"
+            @click="dateButton(n - 1)"
+          >
             {{ getDate(n - 1) }}
           </el-button>
         </div>
         <div class="site-select">
           <div>
-            <el-table class="my_table" :data="SiteTimeEnum" style="width: 100%"
-                      :cell-style="{padding:'2px 0px 2px 0px'}">
-              <el-table-column prop="time" label="Date" width="100" index="0">
+            <el-table
+              class="my_table"
+              :data="SiteTimeEnum"
+              style="width: 100%"
+              :cell-style="{ padding: '2px 0px 2px 0px' }"
+            >
+              <el-table-column prop="time" label="Date" width="100">
                 <template #default="scope">
-                  <div v-if="scope.$index ==0" class="time-enum-start">
+                  <div v-if="scope.$index == 0" class="time-enum-start">
                     {{ getTimeEnumStart(SiteTimeEnum[0].time) }}
                   </div>
                   <div class="time-enum">
@@ -42,9 +55,13 @@
                   </div>
                 </template>
               </el-table-column>
-              <el-table-column v-for="(column, index) in SiteDetail" :label="column.site_name" :key="index"
-                               :prop="column" :highlight-current-row="true"
-                               width="130">
+              <el-table-column
+                v-for="(column, index) in SiteDetail"
+                :label="column.site_name"
+                :key="index"
+                :highlight-current-row="true"
+                width="130"
+              >
                 <template #default="scope">
                   <div>
                     <!--                                                            {{ scope.row.time }}-->
@@ -52,21 +69,20 @@
                     <!--                               style="width: 110px;height: 55px;color: #9E9E9E">￥100-->
                     <!--                    </el-button>-->
                     <!--                    类型：1:时间段过了  2:线上  3:线下-->
-                    <CanChoose :money="getMoney(scope.row.time,scope.column.property)"
-                               :status="getStatus(scope.row.time,scope.column.property)"
-                               @click="updateSiteDetail(scope.column.property,scope.row.time)"
-                               style="width: 110px;height: 55px"/>
+                    <CanChoose
+                      :money="getMoney(scope.row.time, column)"
+                      :status="getStatus(scope.row.time, column)"
+                      @click="updateSiteDetail(column, scope.row.time)"
+                      style="width: 110px; height: 55px"
+                    />
                     <!--                    <div>{{getMoney(scope.row.time,scope.column.property)}}</div>-->
                   </div>
                 </template>
               </el-table-column>
             </el-table>
           </div>
-
-
         </div>
       </div>
-
     </div>
 
     <div class="right">
@@ -74,13 +90,13 @@
         <h3>预约信息</h3>
         <el-form :model="form">
           <el-form-item label="姓名">
-            <el-input v-model="form.name"/>
+            <el-input v-model="form.name" />
           </el-form-item>
           <el-form-item label="手机">
-            <el-input v-model="form.phone"/>
+            <el-input v-model="form.phone" />
           </el-form-item>
           <el-form-item label="备注">
-            <el-input v-model="form.remake"/>
+            <el-input v-model="form.remake" />
           </el-form-item>
         </el-form>
       </div>
@@ -88,44 +104,52 @@
         <h3>场次信息</h3>
 
         <div v-for="item in SiteSelectDetail">
-          <div v-if="item.time_enum.length!=0">
+          <div v-if="item.time_enum.length != 0">
             <div class="right-middle-show">
-              <div class="right-middle-show-top">线下预约:{{ getSiteName(item) }}</div>
+              <div class="right-middle-show-top">
+                线下预约:{{ getSiteName(item) }}
+              </div>
               <div class="right-middle-show-bottom">
-                <div style="color:#9E9E9E;">今天 12-07 共{{ getSiteCount(item) }}场</div>
-                <div style="color: #F44336;font-size: 17px">￥{{ getSiteSumMoney(item) }}</div>
+                <div style="color: #9e9e9e">
+                  今天 12-07 共{{ getSiteCount(item) }}场
+                </div>
+                <div style="color: #f44336; font-size: 17px">
+                  ￥{{ getSiteSumMoney(item) }}
+                </div>
               </div>
             </div>
           </div>
-
         </div>
-
       </div>
 
       <div class="bill">
         <div class="bill-top">
           <h3>合计</h3>
-          <h3 style="color: #F44336">￥{{ getOrderSumMoney() }}</h3>
+          <h3 style="color: #f44336">￥{{ getOrderSumMoney() }}</h3>
         </div>
-        <el-button @click="submit()" type="primary" style="width: 100%;height: 40%">结算</el-button>
+        <el-button
+          @click="submit()"
+          type="primary"
+          style="width: 100%; height: 40%"
+          >结算</el-button
+        >
       </div>
     </div>
   </div>
-
 </template>
 <script setup lang="ts">
-import {onMounted, reactive, ref} from "vue";
-import {SaasAddOrder, SaasGetSiteReserve} from "@/api/site";
-import {useEnumStore} from "@/stores/enum.ts";
-import CanChoose from "@/views/site/CanChoose.vue";
-import {ElMessage} from "element-plus";
-import {Check} from "@element-plus/icons-vue";
+import { onMounted, reactive, ref } from 'vue'
+import { SaasAddOrder, SaasGetSiteReserve } from '@/api/site'
+import { useEnumStore } from '@/stores/enum.ts'
+import CanChoose from '@/views/site/CanChoose.vue'
+import { ElMessage } from 'element-plus'
+import { Check } from '@element-plus/icons-vue'
 
 var SiteTimeEnum = ref<any>([])
 var SiteDetail = ref<any>([])
 let TimeEnum = useEnumStore()
-var currentHour = (new Date()).getHours();
-const today = new Date();
+var currentHour = new Date().getHours()
+const today = new Date()
 var SiteSelectDetail = ref<any>([])
 let form = reactive({
   name: '',
@@ -137,31 +161,36 @@ let selectDate = ''
 //按钮颜色
 let upBtn = 0
 
-
 function getDate(aa: any) {
   window.setTimeout(function () {
     window.requestAnimationFrame(getDate)
   }, 1000 / 2)
-  var d = new Date(today.getTime() + 24 * aa * 60 * 60 * 1000);
-  var month: any = d.getMonth() + 1;  //获取月，从 Date 对象返回月份 (0 ~ 11)，故在此处+1
-  var day: any = d.getDay()    //获取日
+  var d = new Date(today.getTime() + 24 * aa * 60 * 60 * 1000)
+  var month: any = d.getMonth() + 1 //获取月，从 Date 对象返回月份 (0 ~ 11)，故在此处+1
+  var day: any = d.getDay() //获取日
   var days: any = d.getDate() //获取日期
 
-  if (month < 10) month = "0" + month
-  if (days < 10) days = "0" + days
+  if (month < 10) month = '0' + month
+  if (days < 10) days = '0' + days
 
-  var week = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+  var week = [
+    '星期日',
+    '星期一',
+    '星期二',
+    '星期三',
+    '星期四',
+    '星期五',
+    '星期六',
+  ]
 
-  var a = month + "-" + days
+  var a = month + '-' + days
   var c = week[day]
 
   if (aa == 0) {
-    return "今天 " + a
+    return '今天 ' + a
   } else {
-    return c + " " + a
+    return c + ' ' + a
   }
-
-
 }
 
 onMounted(() => {
@@ -176,21 +205,21 @@ function dateButton(n: any) {
   //清空选择列表
   SiteSelectDetail = ref<any>([])
   upBtn = n + 1
-  const today = new Date(new Date().getTime() + 24 * n * 60 * 60 * 1000);
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
-  const thisDayDate = `${year}-${month}-${day}`; //打印当前日期
+  const today = new Date(new Date().getTime() + 24 * n * 60 * 60 * 1000)
+  const year = today.getFullYear()
+  const month = String(today.getMonth() + 1).padStart(2, '0')
+  const day = String(today.getDate()).padStart(2, '0')
+  const thisDayDate = `${year}-${month}-${day}` //打印当前日期
   selectDate = thisDayDate
   infoData(thisDayDate)
 }
 
 function getTodayDate() {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
-  const thisDayDate = `${year}-${month}-${day}`; //打印当前日期
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = String(today.getMonth() + 1).padStart(2, '0')
+  const day = String(today.getDate()).padStart(2, '0')
+  const thisDayDate = `${year}-${month}-${day}` //打印当前日期
   return thisDayDate
 }
 
@@ -207,7 +236,7 @@ function getOrderSumMoney() {
 function infoData(date: any) {
   SiteDetail.value = []
   SiteTimeEnum.value = []
-  SaasGetSiteReserve({shop_id: 1, date: date}).then((res: any) => {
+  SaasGetSiteReserve({ shop_id: 1, date: date }).then((res: any) => {
     //初始时间枚举
     var timeEnum = ref<any>([])
     res.data.data.forEach((value: any) => {
@@ -223,19 +252,18 @@ function infoData(date: any) {
       })
     })
 
-    const uniqueArr = [...new Set(timeEnum.value)];
-    const sortedArr = uniqueArr.sort((a: any, b: any) => a - b);
+    const uniqueArr = [...new Set(timeEnum.value)]
+    const sortedArr = uniqueArr.sort((a: any, b: any) => a - b)
     sortedArr.forEach((vv: any) => {
-      SiteTimeEnum.value.push({time: vv})
+      SiteTimeEnum.value.push({ time: vv })
     })
     //处理价格信息
-
   })
 }
 
 function getStatus(time: any, data: any) {
   var status = 0
-  if (currentHour >= TimeEnum.Enum.time_enum[time].split(":")[0]) {
+  if (currentHour >= TimeEnum.Enum.time_enum[time].split(':')[0]) {
     status = 1
   }
   if (data.saas_reserve_time_enum.length != 0) {
@@ -259,7 +287,6 @@ function getSiteName(site: any) {
   return site.site_name
 }
 
-
 function getSiteCount(site: any) {
   return site.time_enum.length
 }
@@ -279,9 +306,7 @@ function getSiteSumMoney(site: any) {
     }
   })
   return totalMoney / 100
-
 }
-
 
 // {
 //      "user_name":"散客",
@@ -311,24 +336,30 @@ function submit() {
   })
   SiteSelectDetail.value.forEach((value: any) => {
     if (value.time_enum.length > 0) {
-      data.site_detail.push({site_id: value.site_id, time_enum: value.time_enum})
+      data.site_detail.push({
+        site_id: value.site_id,
+        time_enum: value.time_enum,
+      })
     }
   })
   SaasAddOrder(data).then((res: any) => {
     if (res.data.code == 200) {
       ElMessage({
-        type: "success",
-        message: "提交成功了，需要付钱:" + res.data.data.money
+        type: 'success',
+        message: '提交成功了，需要付钱:' + res.data.data.money,
       })
     }
   })
-
 }
 
 function updateSiteDetail(site: any, timeEnum: any) {
   let jump = 0
   if (SiteSelectDetail.value.length == 0) {
-    SiteSelectDetail.value.push({site_id: site.site_id, site_name: site.site_name, time_enum: [timeEnum]})
+    SiteSelectDetail.value.push({
+      site_id: site.site_id,
+      site_name: site.site_name,
+      time_enum: [timeEnum],
+    })
   } else {
     SiteSelectDetail.value.forEach((value: any, index: any) => {
       if (value.site_id == site.site_id) {
@@ -344,10 +375,13 @@ function updateSiteDetail(site: any, timeEnum: any) {
       }
     })
     if (jump == 0) {
-      SiteSelectDetail.value.push({site_id: site.site_id, site_name: site.site_name, time_enum: [timeEnum]})
+      SiteSelectDetail.value.push({
+        site_id: site.site_id,
+        site_name: site.site_name,
+        time_enum: [timeEnum],
+      })
     }
   }
-
 }
 
 function getMoney(time: any, data: any) {
@@ -362,11 +396,11 @@ function getMoney(time: any, data: any) {
 }
 
 function getTimeEnum(time: any) {
-  return TimeEnum.Enum.time_enum[time].split("~")[1]
+  return TimeEnum.Enum.time_enum[time].split('~')[1]
 }
 
 function getTimeEnumStart(time: any) {
-  return TimeEnum.Enum.time_enum[time].split("~")[0]
+  return TimeEnum.Enum.time_enum[time].split('~')[0]
 }
 </script>
 
@@ -374,7 +408,6 @@ function getTimeEnumStart(time: any) {
 .my_table >>> .el-table__body tr:hover > td {
   background-color: #fff !important;
 }
-
 </style>
 <style lang="scss">
 //.el-table__body{
@@ -388,7 +421,6 @@ function getTimeEnumStart(time: any) {
   background-color: #e5e5e5;
   overflow-y: hidden;
 
-
   .left {
     width: 80%;
     height: 100%;
@@ -400,7 +432,7 @@ function getTimeEnumStart(time: any) {
       padding-left: 30px;
       display: flex;
       align-items: center;
-      background-color: #FFFFFF;
+      background-color: #ffffff;
       margin: 10px;
 
       .example-container {
@@ -408,20 +440,20 @@ function getTimeEnumStart(time: any) {
         height: 40%;
         margin-left: 2%;
         margin-right: 1%;
-        background-color: #0077FF;
+        background-color: #0077ff;
 
         .tick {
           position: absolute;
           top: 0;
           right: 0;
           margin-top: -5px;
-          color: #0077FF;
+          color: #0077ff;
         }
       }
 
       .example-container-chose {
         border: solid 1.5px;
-        border-color: #0077FF;
+        border-color: #0077ff;
         position: relative;
       }
 
@@ -429,11 +461,10 @@ function getTimeEnumStart(time: any) {
         font-size: 13px;
         margin-top: 3px;
       }
-
     }
 
     .left-main {
-      background-color: #FFFFFF;
+      background-color: #ffffff;
       margin: 10px;
       height: 100%;
       width: 100%;
@@ -447,7 +478,7 @@ function getTimeEnumStart(time: any) {
         .active {
           color: #1890ff;
           border-color: #badeff;
-          background-color: #e8f4ff
+          background-color: #e8f4ff;
         }
       }
 
@@ -476,13 +507,11 @@ function getTimeEnumStart(time: any) {
           --el-table-border-color: '#FFFFFF' !important;
         }
       }
-
     }
-
   }
 
   .right {
-    background-color: #FFFFFF;
+    background-color: #ffffff;
     width: 20%;
     height: 100%;
     padding: 20px;
@@ -501,7 +530,7 @@ function getTimeEnumStart(time: any) {
         padding: 10px 25px 10px 25px;
         width: 100%;
         height: 8%;
-        background-color: #EFF3FF;
+        background-color: #eff3ff;
         margin-bottom: 20px;
 
         .right-middle-show-top {
@@ -537,8 +566,5 @@ function getTimeEnumStart(time: any) {
       }
     }
   }
-
 }
-
-
 </style>
